bf42d9093e03ad2da3adc1cce7a7847ee6c9c1e5
전종복 <relate16@naver.com>
2023-02-11 12:34:18 +0900

    temp/ 임시저장

--- src/main/java/com/example/meet/controller/LoginController.java
+++ src/main/java/com/example/meet/controller/LoginController.java
@@ -7,6 +7,9 @@ import com.sun.org.apache.xpath.internal.operations.Mod;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,7 +37,16 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String postLogin(@ModelAttribute MemberDto memberDto, HttpServletRequest request, Model model) {
+    public String postLogin(@ModelAttribute MemberDto memberDto, BindingResult bindingResult, HttpServletRequest request, Model model) {
+        // 입력 validation
+        if (!StringUtils.hasText(memberDto.getUsername())) {
+            bindingResult.addError(
+                    new FieldError("member", "username", "username은 필수입니다."));
+        }
+        if (!StringUtils.hasText(memberDto.getPassword())) {
+            bindingResult.addError(
+                    new FieldError("member", "username", "password는 필수입니다."));
+        }
 
         Optional<Member> memberOpt = memberRepository.findByUsername(memberDto.getUsername());
 
