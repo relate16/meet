1f66105b041576d1d46d59b2b778f5112cdb783a
전종복 <relate16@naver.com>
2023-03-08 16:13:26 +0900

    temp/ 임시저장

--- src/main/java/com/example/meet/WebSocketConfig.java
+++ src/main/java/com/example/meet/WebSocketConfig.java
@@ -1,6 +1,6 @@
 package com.example.meet;
 
-import com.example.meet.controller.MultiChatController;
+import com.example.meet.handler.MultiChatHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -14,15 +14,16 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 public class WebSocketConfig implements WebSocketConfigurer {
     // 관련 소스 링크 : https://dev-gorany.tistory.com/212
 
-    private final MultiChatController multiChatController;
+    private final MultiChatHandler multiChatHandler;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        // paths: "ws/~" - 웹소켓은 클라이언트가 접속 요청을 하고 웹 서버가 응답한 후 연결을 끊는 것이 아닌
+        // paths: "ws://localhost:8080/multi-chat" 으로 커넥션을 연결하고 메세지 통신을 할 수 있다.
+        //      "ws/~" - 웹소켓은 클라이언트가 접속 요청을 하고 웹 서버가 응답한 후 연결을 끊는 것이 아닌
         //      Connection을 그대로 유지하고 클라이언트의 요청 없이도 데이터를 전송할 수 있는 프로토콜이다.
         //      프로토콜의 요청은 [ws://~]로 시작한다.
         // .setAllowedorigins("*") : 도메인이 접속 허용할 서버를 추가해준다.
         //      도메인이 다른 서버에서도 접속 가능하도록 모든 경로 "*"를 파라미터에 넣어준다.
-        registry.addHandler(multiChatController, "ws/multiChat").setAllowedOrigins("*");
+        registry.addHandler(multiChatHandler, "ws/multi-chat").setAllowedOrigins("*");
     }
 }
--- src/main/java/com/example/meet/controller/MultiChatController.java
+++ src/main/java/com/example/meet/controller/MultiChatController.java
@@ -2,49 +2,15 @@ package com.example.meet.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.socket.CloseStatus;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
-import org.springframework.web.socket.handler.TextWebSocketHandler;
-import java.util.List;
-import java.util.ArrayList;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Slf4j
-public class MultiChatController extends TextWebSocketHandler {
-    //관련 소스 링크 : https://dev-gorany.tistory.com/212
-    // TextWebSocketHandler 설명 링크 : https://supawer0728.github.io/2018/03/30/spring-websocket/
+public class MultiChatController {
 
-    private static List<WebSocketSession> list = new ArrayList<>(); // 채팅에 접속한 세션들 저장소인듯
-
-    @Override
-    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        //afterConnectionEstablished(WebSocketSession) : connection이 맺어진 후 실행된다.
-        // 즉 Client가 접속시 실행되는 메서드이다.
-
-        list.add(session);
-        log.info("클라이언트 접속 {}", session);
-
-    }
-
-    @Override
-    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        // handleMessage(WebSocketSession, WebSocketMessage<?>) : session에서 메시지를 수신했을 때 실행된다.
-        // message 타입에 따라 handleTextMessage(), handleBinaryMessage()를 실행한다.
-        String payload = message.getPayload();
-        // payload는 전송되는 데이터를 의미한다. 즉 Client에서 보낸 메세지(data)이다.
-        // message 객체 = {payload: "hello"} 와 같은 것 같다(추측)
-
-
-
-    }
-
-    @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        // afterConnectionClosed(WebSocketSession, CloseStatus) : connectino이 종료된 후 실행된다.
-        // 즉, Client가 접속해제시 실행되는 메서드이다.
-
-        list.remove(session);
-        log.info("클라이언트 접속 해제 {}", session);
+    @GetMapping("/multi-chat")
+    public String GetMultiChat() {
+        log.info("multi-chat start");
+        return "multi-chat";
     }
 }
